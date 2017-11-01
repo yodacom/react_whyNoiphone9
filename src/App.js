3f51f44747ba026f311f99e6e1ff7f15c6d04667
Jeremy Black <jb@yodacom.com>
2017-11-01 07:51:30 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: "",
+      realAnswer: ""
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">What Happened to iPhone 9?</h1>
-        </header>
+        <h1 className="App-title">What Happened to iPhone 9?</h1>
+
+        <h2>Answer:{this.state.answer} </h2>
+        <h2>Explain Answer:{this.state.realAnswer} </h2>
+
+        <button onClick={() => this.setState({ answer: "Because 7 8 9" })}>
+          What is the Answer?
+        </button>
+
+        <br />
+
+        <button
+          onClick={() => this.setState({ realAnswer: " Because 7 'ATE' 9 " })}
+        >
+          What is the real Answer?
+        </button>
       </div>
     );
   }
